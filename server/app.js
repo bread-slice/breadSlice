b14a54f7d96d6e8cd53bb41be8ff51ae8d01f02e
Monroe <mgetz10@gmail.com>
2019-04-12 10:23:11 -0400

    tesseract added

--- server/app.js
+++ server/app.js
@@ -5,12 +5,12 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-
+let hbs = require('hbs');
 let indexRouter = require('./routes/index');
-let authRouter  = require('./routes/auth');
-
+let authRouter = require('./routes/auth');
 
 var app = express();
+app.set('view engine', 'hbs');
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
@@ -27,8 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/auth', authRouter)
-
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const uploadCloud = require('../config/cloudinary');
+const Tesseract = require('tesseract.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +9,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/post', uploadCloud.single('photo'), (req, res, next) => {
-  console.log('i made a bitch', req.body);
+  console.log('i made a bitch', req.file.url);
+  console.log(req.file);
+  let myImage = req.file.url;
+  Tesseract.recognize(myImage).then(function(result) {
+    console.log('result!!!!', result.text);
+    res.json({ data: result.text });
+  });
 });
 
 module.exports = router;
